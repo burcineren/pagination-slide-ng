3e151a671f796edf7a1b78b3b8346ea4b893dc92
burcineren <burcin.ern969@gmail.com>
2022-07-18 20:12:48 +0300

    pagination size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,8 +23,48 @@ export class AppComponent {
     {
       title: 'Mardin',
       url: 'https://images.unsplash.com/photo-1646146775038-152e8b8d3ba4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      title: 'Belgrad Ormanı',
+      url: 'https://images.unsplash.com/photo-1642104039745-46a75de8fb04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+    },
+    {
+      title: 'Bali',
+      url: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=438&q=80'
+    },
+    {
+      title: 'Mardin',
+      url: 'https://images.unsplash.com/photo-1646146775038-152e8b8d3ba4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      title: 'Belgrad Ormanı',
+      url: 'https://images.unsplash.com/photo-1642104039745-46a75de8fb04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+    },
+    {
+      title: 'Bali',
+      url: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=438&q=80'
+    },
+    {
+      title: 'Mardin',
+      url: 'https://images.unsplash.com/photo-1646146775038-152e8b8d3ba4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      title: 'Belgrad Ormanı',
+      url: 'https://images.unsplash.com/photo-1642104039745-46a75de8fb04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+    },
+    {
+      title: 'Bali',
+      url: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=438&q=80'
+    },
+    {
+      title: 'Mardin',
+      url: 'https://images.unsplash.com/photo-1646146775038-152e8b8d3ba4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
     }
   ]
+  pageIndex(index : number)
+  {
+    return Math.abs(this.currentPage - index);
+  }
   // chengePage(number : number){
   //   this.currentPage =number;
   // }
